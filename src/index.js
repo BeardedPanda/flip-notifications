137c56f7f3670f0ff4e5de1cdad592b6665cd44c
BeardedPanda <sergii.kleban@gmail.com>
2017-12-07 17:30:12 +0200

    Code refactor

--- src/index.js
+++ src/index.js
@@ -31,8 +31,18 @@ class FlipNotifications {
      * Show method appends notification wrapper to body
      */
     show() {
+        /**
+         * Generate notification html code
+         */
         this.setUpWrapperHtml();
+        /**
+         * Add notification to DOM
+         */
         document.body.appendChild(this.wrapper);
+        /**
+         * Recalculate notifications offset heights
+         */
+        this.resetOffsetHeights();
     }
 
     /**
@@ -42,7 +52,6 @@ class FlipNotifications {
         this.wrapper.className = `notification-wrapper is-animated is-attached-${this.attachment_direction} is-active is-${this.type}`;
         this.wrapper.innerHTML = `<aside class="icon-wrapper">${this.getTypeIconCode()}</aside>`;
         this.wrapper.innerHTML += this.url ? ` <aside class="text"><p>${this.message}</p><p class="url-wrapper"><a href="${this.url}">» View</a></p></aside>` : ` <aside class="text"><p>${this.message}</p></aside>`;
-        this.setWrapperOffsetHeight();
         this.setUpDismissButton();
     }
 
@@ -62,20 +71,6 @@ class FlipNotifications {
             default:
                 return '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 459.334 459.334" style="enable-background:new 0 0 459.334 459.334;" xml:space="preserve"><g><path d="M175.216,404.514c-0.001,0.12-0.009,0.239-0.009,0.359c0,30.078,24.383,54.461,54.461,54.461 s54.461-24.383,54.461-54.461c0-0.12-0.008-0.239-0.009-0.359H175.216z"/><path d="M403.549,336.438l-49.015-72.002c0-22.041,0-75.898,0-89.83c0-60.581-43.144-111.079-100.381-122.459V24.485 C254.152,10.963,243.19,0,229.667,0s-24.485,10.963-24.485,24.485v27.663c-57.237,11.381-100.381,61.879-100.381,122.459 c0,23.716,0,76.084,0,89.83l-49.015,72.002c-5.163,7.584-5.709,17.401-1.419,25.511c4.29,8.11,12.712,13.182,21.887,13.182 H383.08c9.175,0,17.597-5.073,21.887-13.182C409.258,353.839,408.711,344.022,403.549,336.438z"/></g></svg>';
         }
-    }
-
-    /**
-     * Set offset top height
-     */
-    setWrapperOffsetHeight() {
-        let active_notifications = document.querySelectorAll(`.notification-wrapper.is-animated.is-attached-${this.attachment_direction}.is-active`);
-        let offset_top = 0;
-        if(active_notifications.length) {
-            active_notifications.forEach(item => {
-                offset_top += item.clientHeight;
-            })
-        }
-        this.wrapper.style.top = offset_top + 'px';
     }
 
     /**
